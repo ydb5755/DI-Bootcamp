b7ad14d04587d3e174ff82c5caf22fefb888ce68
ydb5755 <yisroel.d.baum@gmail.com>
2023-01-18 11:59:42 +0200

    ExercisesXPNinja

---
+++ Week-3/Day-4/ExercisesXPNinja/script.js
@@ -0,0 +1,41 @@
+// Exercise 1 : Age Difference
+// Instruction
+// Given the years two people were born, find the date when the younger one is exactly half the age of the older.
+// Notes: The dates are given in the format YYYY
+// let age1 = prompt("Please input the year that the younger person  was born, in this format YYYY");
+// let age2 = prompt("Please input the year that the older person was born, in this format YYYY");
+// let year = +age1 - +age2 + +age1; 
+// alert(year);
+
+// Exercise 2 : Zip Codes
+// Instruction
+// Harder exercise
+// Hint : This exercise has 2 parts. First, do this exercise without Regular Expressions, then do it using Regular Expressions
+
+// While working in a Post Office you must have the clients’ zip code in order to send them their letters.
+// Ask the client for their zip code and console.log “success” or “error” based on the following rules.
+// Zip codes consists of 5 numbers
+// Must only contain numbers
+// Must not contain any whitespace (spaces)
+// Must not be greater than 5 digits in length
+
+// let zip = prompt("Enter zip code of only 5 numbers");
+// if (zip.length == 5 && isNaN(zip) == false && zip.indexOf(" ") == -1){
+//     alert("success");
+// } else{
+//     alert("error");
+// }
+
+// Exercise 3 : Secret Word
+// Instruction
+// Harder exercise
+// Hint : Use Regular Expressions
+
+// Prompt the user for a word and save it to a variable.
+// Delete all the vowels of the word and console.log the result.
+// Bonus: Replace the vowels with another character and console.log the result
+// a = 1
+// e = 2
+// i = 3
+// o = 4
+// u = 5
