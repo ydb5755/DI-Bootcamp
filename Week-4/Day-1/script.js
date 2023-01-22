b9b50fd01f1f2062bba322c23fd20a1945334d5b
ydb5755 <yisroel.d.baum@gmail.com>
2023-01-22 20:45:54 +0200

    week4

---
+++ Week-4/Day-1/script.js
@@ -0,0 +1,10 @@
+function myAge (age){
+    let mumage = age * 2;
+    let dadage = mumage * 1.2;
+
+    // console.log(dadage);
+    // console.log(mumage);
+    return mumage;
+}
+let age = myAge(27);
+console.log(age);
