d4642694233a1239e9caad5ee64c475dd8b27936
yisroelBaum <yisroel.d.baum@gmail.com>
2023-02-06 11:12:17 +0200

    python exercise and some previous week work

--- Week-5/Day-3/script.js
+++
@@ -1,6 +0,0 @@
-
-var div = document.createElement('div');
- 
- 
-if ('draggable' in div || ('ondragstart' in div && 'ondrop' in div))
-    console.log("Drag and Drop API is supported!");
---
+++ Week-6/Day-2/index.py
@@ -0,0 +1,51 @@
+#Exercise1
+# print("Hello world\nHello world\nHello world\nHello world")
+
+#Exercise 2
+# print(99**3*8)
+
+#Exercise3
+# 5 < 3 #false
+# 3 == 3 #true
+# 3 == "3" #false
+# "3" > 3 #error
+# "Hello" == "hello"#false
+
+# Exercise 4
+# computer_brand = "thinkpad"
+# print(f'i have a {computer_brand} computer')
+
+# Exercise5
+# name = "yisroel baum"
+# age = '27'
+# shoe_size = "40"
+# info = f'Hi my name is {name} i am {age} years old and i wear size {shoe_size} shoes'
+# print(info)
+
+# Exercise 6
+# a = 10
+# b = 5
+# if a > b:
+#     print('Hello World')
+
+# Exercise 7
+# num = int(input("please enter a number"))
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# Exercise 8
+# my_name = "yisroel"
+# your_name = input("please enter your name")
+# if my_name == your_name:
+#     print("wait but thats... my... name.... wait a minute, who are you? who am i?")
+# else:
+#     print("wow thats a cool name")
+
+# Exercise 9
+height = int(input("enter your height in cm"))
+if height >= 145:
+    print('youre tall enough to ride, good for you')
+else:
+    print('ha shorty, youre too short to ride, grow some more then come back')
