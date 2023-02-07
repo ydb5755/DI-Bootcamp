3fc8725bb0f589c9d2e774a995376be79cb58435
yisroelBaum <yisroel.d.baum@gmail.com>
2023-02-07 20:42:42 +0200

    class practice

---
+++ Week-6/Day-3/index.py
@@ -0,0 +1,32 @@
+
+
+
+# list1 = [5, 10, 15, 20, 25, 50, 20]
+# change = list1.index(20) 
+# list1[change] = 200
+
+
+# print(list1)
+# a_tuple = (10, 20, 30, 40)
+# a,b,c,d = a_tuple
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# numbers = [3, 5, 1, 2]
+# numbers.sort()
+# print(numbers)
+
+# num = int(input("enter a number to be multiplied"))
+# list = [1,2,3,4,5,6,7,8,9,10]
+# for numbr in list:
+#     multiplied = num * numbr
+#     print(f'{num} times {numbr} equals {multiplied}')
+# num = int(input("enter a number to be multiplied"))
+# for numbr in range(0,11):
+#     multiplied = num * numbr
+#     print(f'{num} times {numbr} equals {multiplied}')
+num = 1
+while num < 11:
+    print(num)
+    num += 1
