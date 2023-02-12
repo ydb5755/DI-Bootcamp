677d4132679a7e9eef68afa792f4df22626dae23
yisroelBaum <yisroel.d.baum@gmail.com>
2023-02-12 22:29:51 +0200

    daily challenge

---
+++ Week-7/Day-2/Daily-Challenge/index.py
@@ -0,0 +1,25 @@
+    
+import re
+matrix = [
+    ['7','i','3'],
+    ['T','s','i'],
+    ['h','%','x'],
+    ['i','','#'],
+    ['s','M',''],
+    ['$','a',''],
+    ['#','t','%'],
+    ['^','r','!']
+]
+arr = []
+for index1, unit in enumerate(matrix):
+    arr.append(matrix[index1][0])
+for index1, unit in enumerate(matrix):
+    arr.append(matrix[index1][1])
+for index1, unit in enumerate(matrix):
+    arr.append(matrix[index1][2])
+newlist = [re.sub(r'[^a-zA-Z]+', '', x) for x in arr]
+for item in newlist:
+    if item == '':
+        newlist.remove(item)
+final = ''.join(newlist)
+print(final)
