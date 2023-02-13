78ee5f88fcfa0b6fb9a05df2fc69c293e131d657
yisroelBaum <yisroel.d.baum@gmail.com>
2023-02-13 09:07:37 +0200

    better solution

--- Week-7/Day-2/Daily-Challenge/index.py
+++ Week-7/Day-2/Daily-Challenge/index.py
@@ -10,23 +10,36 @@ matrix = [
     ['#','t','%'],
     ['^','r','!']
 ]
+
+
 arr = []
-for index1, unit in enumerate(matrix):
-    arr.append(matrix[index1][0])
-for index1, unit in enumerate(matrix):
-    arr.append(matrix[index1][1])
-for index1, unit in enumerate(matrix):
-    arr.append(matrix[index1][2])
-# newlist = [re.sub(r'[^a-zA-Z]+', '', x) for x in arr]
-# for item in newlist:
-#     if item == '':
-#         newlist.remove(item)
-# final = ''.join(newlist)
-final = []
-for unit in arr:
-    if unit.isalpha():
-        final.append(unit)
-    else:
-        final.append(' ')
-final = ''.join(final)
+for item in range(0, len(matrix[0])):
+    for unit in range(0,len(matrix)):
+        arr.append(matrix[unit][item])
+
+
+
+# final = []
+# for unit in arr:
+#     if unit.isalpha():
+#         final.append(unit)
+#     else:
+#         final.append(' ')
+# final = ''.join(final)
+# print(final)
+
+
+#Below is part of another solution to this problem using regex, this results in ThisisMatrix with no spaces
+newlist = [re.sub('[^a-zA-Z]+', '', x) for x in arr]
+final = ' '.join(newlist)
 print(final)
+
+
+#Longer way of making the same result as lines 15-18
+# arr = []
+# for index1, unit in enumerate(matrix):
+#     arr.append(matrix[index1][0])
+# for index1, unit in enumerate(matrix):
+#     arr.append(matrix[index1][1])
+# for index1, unit in enumerate(matrix):
+#     arr.append(matrix[index1][2])
