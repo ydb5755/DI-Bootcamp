ea0b722252daaa19b8d0640aebe8b7002b3a9c9e
yisroelBaum <yisroel.d.baum@gmail.com>
2023-02-19 11:37:50 +0200

    sorting alphabetically

---
+++ Week-7/Day-5/sorting.py
@@ -0,0 +1,19 @@
+# Instructions
+# Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Use List Comprehension
+# Example:
+
+# Suppose the following input is supplied to the program: without,hello,bag,world
+# Then, the output should be: bag,hello,without,worl
+
+
+
+def sorting(str):
+    lowercase_str = str.lower()
+    split_list = lowercase_str.split(',')
+    sorted_list = sorted(split_list)
+    sorted_string = (',').join(sorted_list)
+    return sorted_string
+
+
+print(sorting('without,hello,bag,world'))
