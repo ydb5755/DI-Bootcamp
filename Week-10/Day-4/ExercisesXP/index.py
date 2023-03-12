ac63e011a01aade8364be53178411437f2b947a8
yisroelBaum <yisroel.d.baum@gmail.com>
2023-03-12 19:12:03 +0200

    update

---
+++ Week-10/Day-4/ExercisesXP/index.py
@@ -0,0 +1,48 @@
+
+# 🌟 Exercise 1 – Random Sentence Generator
+# Instructions
+# Description: In this exercise we will create a random sentence generator. We will do this by asking the user how long the sentence should be and then printing the generated sentence.
+
+# Hint : The generated sentences do not have to make sense.
+
+# Download this word list
+
+# Save it in your development directory.
+
+# Create a function called get_words_from_file. This function should read the file’s content and return the words as a collection. What is the correct data type to store the words?
+def get_words_from_file():
+    pass
+# Create another function called get_random_sentence which takes a single parameter called length. The length parameter will be used to determine how many words the sentence should have. The function should:
+# use the words list to get your random words.
+# the amount of words should be the value of the length parameter.
+
+# Take the random words and create a sentence (using a python method), the sentence should be lower case.
+
+# Create a function called main which will:
+
+# Print a message explaining what the program does.
+
+# Ask the user how long they want the sentence to be. Acceptable values are: an integer between 2 and 20. Validate your data and test your validation!
+# If the user inputs incorrect data, print an error message and end the program.
+# If the user inputs correct data, run your code.
+
+
+# 🌟 Exercise 2: Working With JSON
+# Instructions
+# import json
+# sampleJson = """{ 
+#    "company":{ 
+#       "employee":{ 
+#          "name":"emma",
+#          "payable":{ 
+#             "salary":7000,
+#             "bonus":800
+#          }
+#       }
+#    }
+# }"""
+
+
+# Access the nested “salary” key from the JSON-string above.
+# Add a key called “birth_date” to the JSON-string at the same level as the “name” key.
+# Save the dictionary as JSON to a file.
--- Week-10/Day-5/app.py
+++ Week-10/Day-5/app.py
@@ -5,3 +5,7 @@ app = flask.Flask(__name__)
 @app.rout('/')
 def index():
     return 'Hello'
+
+@app.rout('/<username>')
+def index1(username):
+    return f'Hello {username}'
