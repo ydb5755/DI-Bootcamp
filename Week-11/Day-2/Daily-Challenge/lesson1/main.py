2bd804251a53cc5966882ab125791bc2a31b56c9
yisroelBaum <yisroel.d.baum@gmail.com>
2023-03-14 14:07:52 +0200

    update

---
+++ Week-11/Day-2/Daily-Challenge/lesson1/main.py
@@ -0,0 +1,24 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return flask.redirect(flask.url_for('lesson'))
+
+@app.route('/lesson1')
+def lesson():
+    with open('./md-files/in-this-chapter.md') as f:
+        md = f.read()
+    html = flask.render_template('lesson.html', lesson_page=md)
+    return html
+
+@app.route('/exercise')
+def exercise():
+    with open('./md-files/exercises.md') as f:
+        md = f.read()
+    html = flask.render_template('exercises.html', exercise_page=md)
+    return html
+
+if __name__ == '__main__':
+    app.run(debug=True)
