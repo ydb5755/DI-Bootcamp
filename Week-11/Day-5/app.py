321d59e1d5583c0aba234b5c7e1df43cb732ab9a
yisroelBaum <yisroel.d.baum@gmail.com>
2023-03-17 16:29:26 +0200

    Cart App

--- Week-11/Day-5/app.py
+++ Week-11/Day-5/app.py
@@ -1,4 +1,4 @@
-import products_data
+from products_data import retrieve_all_products, retrieve_requested_product
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('homepage.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html', data=retrieve_all_products())
+
+@app.route('/products/<product_id>')
+def product_details(product_id):
+    return render_template('product_details.html', product_id=retrieve_requested_product(product_id), data=retrieve_all_products())
+
+app.run(debug=True)
