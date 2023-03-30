d61500f0ed76c619b944c0adfda9df99a2325c8e
yisroelBaum <yisroel.d.baum@gmail.com>
2023-03-30 12:50:23 +0300

    added check for non-existing results

--- Week-13/Day-4/Phone-number1/phone_app/routes.py
+++ Week-13/Day-4/Phone-number1/phone_app/routes.py
@@ -5,11 +5,17 @@ from flask import render_template
 
 @app.route('/person/<phone_number>')
 def by_number(phone_number):
-    list_of = models.Phone_Number.query.filter_by(number=phone_number).first().owner
-    user = models.Person.query.filter_by(id=list_of).first()
-    return render_template('number.html', user=user)
+    if models.Phone_Number.query.filter_by(number=phone_number).first():
+        list_of = models.Phone_Number.query.filter_by(number=phone_number).first().owner
+        user = models.Person.query.filter_by(id=list_of).first()
+        return render_template('number.html', user=user)
+    else:
+        return 'No results found'
 
 @app.route('/name/<name>')
 def by_name(name):
-    user = models.Person.query.filter_by(name=name).first()
-    return render_template('name.html', user=user)
+    if models.Person.query.filter_by(name=name).first():
+        user = models.Person.query.filter_by(name=name).first()
+        return render_template('name.html', user=user)
+    else:
+        return 'No results found'
