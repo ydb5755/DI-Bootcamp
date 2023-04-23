35812ec777775e7d5192bdf8c59feb356be4ff2d
yisroelBaum <yisroel.d.baum@gmail.com>
2023-04-23 15:24:30 +0300

    finished films

--- Week-15/Day-2/MyApp/app/films/forms.py
+++ Week-15/Day-2/MyApp/app/films/forms.py
@@ -1,17 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateField, SelectField, SelectMultipleField, FormField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, StopValidation
+from wtforms import widgets
 from app import app
 from app.models import Country, Film, Category, Director
 
 with app.app_context():
     all_categories  = Category.query.all()
 # all_categories = []
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(html_tag='ol', prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+class MultiCheckboxAtLeastOne():
+    def __init__(self, message=None):
+        if not message:
+            message = 'At least one option must be selected.'
+        self.message = message
+
+    def __call__(self, form, field):
+        if len(field.data) == 0:
+            raise StopValidation(self.message)
 
 class AddFilmForm(FlaskForm):
     title                  = StringField('Title', validators=[DataRequired()])
     release_date           = DateField('Release Date', validators=[DataRequired()])
-    categories             = SelectField('Categories', choices=[cat.name for cat in all_categories] ,validators=[DataRequired()])
+    categories             = MultiCheckboxField('Categories', choices=[cat.name for cat in all_categories], validators=[MultiCheckboxAtLeastOne()])
     created_in_country     = StringField('Created in Country:', validators=[DataRequired()])
     submit                 = SubmitField('Submit') 
 
@@ -21,4 +35,8 @@ class AddDirectorForm(FlaskForm):
     film       = SelectField('Film', validators=[DataRequired()])
     submit = SubmitField('Submit') 
 
+class UpdateDirectorForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name  = StringField('Last Name', validators=[DataRequired()])
+    submit = SubmitField('Submit') 
  
--- Week-15/Day-2/MyApp/app/films/routes.py
+++ Week-15/Day-2/MyApp/app/films/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for
 from app import app, db
 from app.films import films_bp
-from app.films.forms import AddDirectorForm, AddFilmForm
+from app.films.forms import AddDirectorForm, AddFilmForm, UpdateDirectorForm
 from app.models import Film, Director, Country, Category
 
 
@@ -20,19 +20,18 @@ def addFilm():
         if not country_to_add_to:
             country_to_add_to = Country(name=film_form.created_in_country.data)
 
-        category_to_add_to = Category.query.filter_by(name=film_form.categories.data).first()
-
         film = Film(title=film_form.title.data, 
                     release_date=film_form.release_date.data,
                     created_in_country=country_to_add_to
                     )
-        film.category.append(category_to_add_to)
+        
         country_to_add_to.films_produced.append(film)
         country_to_add_to.save_country()
         
-    
-        category_to_add_to.films.append(film)
-        category_to_add_to.save_category()
+        for cat in film_form.categories:
+            category_to_add_to = Category.query.filter_by(name=cat.data).first()
+            film.category.append(category_to_add_to)
+            category_to_add_to.films.append(film)
 
         film.save_film()
         return redirect(url_for('films_bp.homepage'))
@@ -61,6 +60,43 @@ def addDirector():
     return render_template('addDirector.html', form=form)
 
 
-@films_bp.route('/editDirector/<director_id>')
+@films_bp.route('/chooseDirector/<film_id>')
+def choose_director(film_id):
+    directors = Film.query.filter_by(id=film_id).first().director
+    return render_template('chooseDirector.html', directors=directors)
+
+@films_bp.route('/editDirector/<director_id>', methods=('GET', 'POST'))
 def edit_director(director_id):
-    return render_template('editDirector.html')
+    director_to_update = Director.query.filter_by(id=director_id).first()
+    form = UpdateDirectorForm()
+    if form.validate_on_submit():
+        director_to_update.first_name = form.first_name.data
+        director_to_update.last_name  = form.last_name.data
+        db.session.commit()
+        return redirect(url_for('films_bp.homepage'))
+    return render_template('editDirector.html', 
+                           director_to_update=director_to_update, 
+                           form=form)
+
+@films_bp.route('/editFilm/<film_id>', methods=('GET', 'POST'))
+def edit_film(film_id):
+    film_to_update = Film.query.filter_by(id=film_id).first()
+    form = AddFilmForm()
+    if form.validate_on_submit():
+        film_to_update.title = form.title.data
+        film_to_update.release_date = form.release_date.data
+        film_to_update.category = []
+        if form.categories.data:
+            for cat in form.categories.data:
+                category_to_add_to = Category.query.filter_by(name=cat).first()
+                film_to_update.category.append(category_to_add_to)
+                category_to_add_to.films.append(film_to_update)
+        country_to_add_to = Country.query.filter_by(name=form.created_in_country.data).first()
+        if not country_to_add_to:
+            country_to_add_to = Country(name=form.created_in_country.data)
+            db.session.add(country_to_add_to)
+        film_to_update.country = country_to_add_to
+        country_to_add_to.films_produced.append(film_to_update)
+        db.session.commit()
+        return redirect(url_for('films_bp.homepage'))
+    return render_template('editFilm.html', form=form, film_to_update=film_to_update)
