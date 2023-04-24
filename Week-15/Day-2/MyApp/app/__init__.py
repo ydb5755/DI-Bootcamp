9308700a27bc4f86951bf59961e08f760a2bd2ad
yisroelBaum <yisroel.d.baum@gmail.com>
2023-04-24 11:40:38 +0300

    update login view config

--- Week-15/Day-2/MyApp/app/__init__.py
+++ Week-15/Day-2/MyApp/app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -14,14 +15,10 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     db.init_app(app)
     migrate.init_app(app,db)
-    login_manager = LoginManager(app)
 
-    # login_manager.init_app(app)
-    login_manager.login_view = "accounts.login"  # type: ignore
-    # @login_manager.login_view
-    # def login():
-    #     # Render the login template.
-    #     return render_template('login.html')
+    login_manager.init_app(app)
+    login_manager.login_view = "accounts_bp.login"  
+
     from .models import User
 
     @login_manager.user_loader
